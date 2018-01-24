e6fdeb0723cd3e7e836004a3613ed0c1127c0782
Adam Bernot <avb100@gmail.com>
2018-01-23 21:03:21 -0500

    Add pace statistic
    Add pace statistic in minutes per mile.

--- js/run-better.js
+++ js/run-better.js
@@ -1,10 +1,10 @@
 function planRace() {
 	//Accepts distance in meters and pace in seconds per mile
     $("#calculator-results").children("tr").remove();
-	
+
 	var secondsPerMile = ($("#race-result-slider").val()) / $("#race-result-distance").val();
 	var secondsPerKilometer = ($("#race-result-slider").val()) / $("#race-result-distance").val() / 1.60934;
-	
+
 	for (i=1;i<=Math.floor( $("#race-result-distance").val() );i++) {
 		for (j=Math.ceil((i-1)*1.60934);j<=Math.floor(i*1.60934);j++) {
 			var kilometerRow = "<tr><td class=\"font-italic\">" + (j/1.60934).toFixed(2) + "</td><td></td><td>" + j + "</td><td>" + shortTimeFormat( secondsPerKilometer*j ) + "</td>";
@@ -34,19 +34,19 @@ function vo2(velocity) {
 
 function vo2max(seconds, miles) {
 	var minutes = parseFloat(seconds)/60.0;
-	
+
 	var velocity = miles * 1609.344 / minutes;
-	
+
 	return vo2(velocity) / percentMax(minutes);
 }
 
 function shortTimeFormat(inputSeconds) {
 		var readout = "";
-		
+
 		var hours = Math.floor( inputSeconds / 3600 );
 		var minutes = Math.floor( (inputSeconds % 3600) / 60);
 		var seconds = Math.floor( inputSeconds % 60 );
-		
+
 		if (hours>0) {
 			readout += hours + ":";
 		}
@@ -59,11 +59,11 @@ function shortTimeFormat(inputSeconds) {
 
 function longTimeFormat(inputSeconds) {
 		var readout = "";
-		
+
 		var hours = Math.floor( inputSeconds / 3600 );
 		var minutes = Math.floor( (inputSeconds % 3600) / 60);
 		var seconds = Math.round( inputSeconds % 60 );
-		
+
 		if (hours>0) {
 			readout += hours + " hour" + (hours>1 ? "s" : "" ) + ", ";
 		}
@@ -80,17 +80,17 @@ $("#race-result-distance").change(
 		var oldMin = $("#race-result-slider").attr("min");
 		var oldMax = $("#race-result-slider").attr("max");
 		var oldVal = $("#race-result-slider").val();
-		
+
 		var sliderPercent = (oldVal-oldMin) / (oldMax-oldMin);
-		
+
 		var newMin = Math.floor( 220 * $("#race-result-distance").val() );
 		var newMax = Math.ceil( 1080 * $("#race-result-distance").val() );
-		
+
 		$("#race-result-slider").attr("min", newMin );
 		$("#race-result-slider").attr("max", newMax );
-		
+
 		$("#race-result-slider").val( sliderPercent * (newMax-newMin) + newMin );
-		
+
 		//Recalculate everything
 		$("#race-result-slider").trigger( "input" );
 	}
@@ -98,14 +98,19 @@ $("#race-result-distance").change(
 
 $("#race-result-slider").on(
 	"input", function() {
-		//Show slider value in text as Race Result
-		$("#race-result-display").text( longTimeFormat( $("#race-result-slider").val() ) );
-		
-		//Calculate VO2 Max of performance
-		var seconds = $("#race-result-slider").val();
+    //Get performance details
+    var seconds = $("#race-result-slider").val();
 		var miles = $("#race-result-distance").val();
+
+		//Show slider value in text as Race Result
+		$("#race-result-display").text( longTimeFormat( seconds ) );
+
+		//Calculate VO2 Max of performance
 		$("#vo2-max").text( vo2max( seconds, miles ).toFixed(2) );
-		
+
+    //Calculate pace of performance
+    $("#pace").text( shortTimeFormat(seconds/miles) );
+
 		//Complete pace chart
 		planRace();
 	}
